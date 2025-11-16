9b8783b966212841001cdf931dcca6886b8f9d92
Ajithesh9 <ajithesh1418@gmail.com>
2025-11-16 19:52:20 +0530

    App.jsx conflict fix

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import { AnimatePresence } from 'framer-motion';
 import Lenis from 'lenis';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
-import Features from './components/features';
+import Features from './components/Features';
 import HowItWorks from './components/HowItWorks';
 import Pricing from './components/Pricing';
 import FAQ from './components/FAQ';
